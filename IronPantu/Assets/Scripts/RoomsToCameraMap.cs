03fe515fd24277139af025be72e58053317285ba
MackanMackan <38558616+MackanMackan@users.noreply.github.com>
2021-11-01 15:05:49 +0100

    added security cam Map script

---
+++ IronPantu/Assets/Scripts/RoomsToCameraMap.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RoomsToCameraMap : MonoBehaviour
+{
+    public Button room;
+    GameObject[] rooms;
+    float buttonRoomWidth;
+    float buttonRoomHeight;
+    float cameraHeight;
+    float cameraWidth;
+    float buttonOffsetX;
+    float buttonOffsetY;
+    void Start()
+    {
+        cameraHeight = Camera.main.scaledPixelHeight;
+        cameraWidth = Camera.main.scaledPixelWidth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void GetCameraMap()
+    {
+        rooms = GameObject.FindGameObjectsWithTag("Room");
+
+        buttonRoomHeight = cameraHeight / rooms.Length;
+        buttonRoomWidth = cameraWidth / rooms.Length;
+
+        foreach(GameObject roomInArray in rooms)
+        {
+            Button roomInstance = Instantiate(room);
+            //TODO: Add offsets for each button to fit the screen correctly;
+            roomInstance.GetComponent<RectTransform>().rect.Set(transform.position.x,transform.position.y, buttonRoomWidth,buttonRoomHeight);
+        }
+    }
+}
