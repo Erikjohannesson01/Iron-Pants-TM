74459c3c6beed8219245fb1fc7cc2dce3e292458
WashFall <carl.christensen98@gmail.com>
2021-11-02 15:26:05 +0100

    Added states

--- IronPantu/Assets/Scripts/TESTRestart.cs
+++ IronPantu/Assets/Scripts/TESTRestart.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class NewBehaviourScript : MonoBehaviour
+public class TESTRestart : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,6 +14,9 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            SceneManager.LoadScene(1);
+        }
     }
 }
